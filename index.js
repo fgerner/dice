2659ff6c140af2da6a00ddaf1788351b66a5649a
Fredrik <fred.gerner@icloud.com>
2020-04-23 21:56:57 +1000

    done

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-
 randomNumber1 = Math.floor(Math.random() * 6) + 1;
+randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-console.log(randomNumber1);
+let first_dice = document.getElementsByClassName('img1')[0];
+let second_dice = document.getElementsByClassName('img2')[0];
+first_dice.src = "./img/dice" + randomNumber1 + ".png";
+second_dice.src = "./img/dice" + randomNumber2 + ".png";
+
+if (randomNumber1 < randomNumber2) {
+    document.querySelector('h1').innerHTML = 'Player two wins!!';
+} else if (randomNumber1 > randomNumber2) {
+    document.querySelector('h1').innerHTML = 'Player one wins!';
+} else {
+    document.querySelector('h1').innerHTML = 'It\'s a tie';
+}
